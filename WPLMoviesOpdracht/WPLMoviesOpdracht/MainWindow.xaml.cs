425e55c9b4bbdf50ce2aec18e385316169dea419
Pajman Nasib <nasib.paymantri@outlook.com>
2024-10-09 18:07:28 +0200

    Double click
    MOVIE-05 : Wanneer de gebruiker dubbel klikt op een afbeelding wordt de titel van de film met een korte beschrijving getoond in een TextBlock onderaan

--- WPLMoviesOpdracht/WPLMoviesOpdracht/MainWindow.xaml.cs
+++ WPLMoviesOpdracht/WPLMoviesOpdracht/MainWindow.xaml.cs
@@ -24,5 +24,20 @@ namespace WPLMoviesOpdracht
         {
             InitializeComponent();
         }
+
+        private void DuneBtn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ResultTxt.Text = "Dune: Part Two\r\nPaul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.";
+        }
+
+        private void FuriosaBtn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ResultTxt.Text = "The Substance\r\nA fading celebrity decides to use a black-market drug, a cell-replicating substance that temporarily creates a younger, better version of herself.";
+        }
+
+        private void lastOneBtn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ResultTxt.Text = "Furiosa: A Mad Max Saga\r\nThe origin story of renegade warrior Furiosa before her encounter and teamup with Mad Max.";
+        }
     }
 }
